fef9b4a6e23a6ce9d99f2fd5a8230d054db8d4b2
박성준 <freestarjun@gmail.com>
2025-10-28 23:37:24 +0900

    main 상단창 개발

---
+++ Apple_Clone/src/pages/main.jsx
@@ -0,0 +1,28 @@
+import '../styles/main.css';
+
+function Main() {
+    return (
+        <>
+            <div className = "Main-Div">
+                <img className = "Main-Apple" src = "./Apple.svg"></img>
+
+                <div className = "Title-Container">
+                    <a>스토어</a>
+                    <a>Mac</a>
+                    <a>iPad</a>
+                    <a>iPhone</a>
+                    <a>Watch</a>
+                    <a>AirPods</a>
+                    <a>TV 및 홈</a>
+                    <a>엔터테인먼트</a>
+                    <a>액세서리</a>
+                    <a>고객지원</a>
+
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Main
