bcdd39913365f7d0a4a4c3c7985f13b8f0c3a205
박성준 <freestarjun@gmail.com>
2025-12-26 23:02:14 +0900

    feat : Watch포스터 개발

--- Apple_Clone/src/pages/main.jsx
+++ Apple_Clone/src/pages/main.jsx
@@ -24,7 +24,7 @@ function Main() {
 
             <section className = "iPhone-Poster">
                     <h1 className = "iPhone">iPhone 17 Pro</h1>
-                    <p>전방위 Pro.</p>
+                    <p className = "introduction-iphone">전방위 Pro.</p>
                 <div className="btn-container">
                     <button className = "first-btn">더 알아보기</button>
                     <button className = "second-btn">구입하기</button>
@@ -35,14 +35,29 @@ function Main() {
             <br></br>
 
             <section className = "AirPods-Poster">
-                    <h1 className = "iPhone">AirPods Pro 3</h1>
-                    <p className='introduction'>세계 최고의 인이어 엑티브 노이즈 캔슬링.</p>
+                    <h1 className = "AirPods">AirPods Pro 3</h1>
+                    <p className='introduction-airpods'>세계 최고의 인이어 엑티브 노이즈 캔슬링.</p>
                 <div className="btn-container">
                     <button className = "first-btn">더 알아보기</button>
                     <button className = "second-btn">구입하기</button>
                 </div>
             </section>
             <img className = "Poster-Img" src = "/AirPods.png" />
+
+            <br></br>
+
+            <section className = "Watch-Poster">
+                    <h1 className = "Watch">
+                    WATCH 
+                    <span> SERIES 11</span>
+                    </h1>
+                    <p className='introduction-watch'>새해의 결심을 꾸준한 습관으로. <br></br>운동 목표. 작심삼일은 이제 그만.</p>
+                <div className="btn-container">
+                    <button className = "first-btn">더 알아보기</button>
+                    <button className = "second-btn">구입하기</button>
+                </div>
+            </section>
+            <img className = "Watch-Img" src = "/Watch.png" />
         </>
 
         
